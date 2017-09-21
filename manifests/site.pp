a1bbb4d3e5e032028bd6a46c1c7b9309c69141ed
root <root@puppetm.indra.com>
2017-09-21 03:07:35 +0000

    t

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,38 +1,5 @@
-node puppeta.indra.com {
-
-class { 'ntp':
-servers => [
-'nist-time-server.eoni.com',
-'nist1-lv.ustiming.org',
-'ntp-nist.ldsbc.edu'
-],
-disable_auth => true,
-logfile => '/var/tmp/ntp.log',
-}
-
-
-class { 'mysql::server':
-root_password => 'strongpassword',
-override_options => {
-'mysqld' => { 'max_connections' => '2024' }
-},
-}
-
-include ::mysql::server::account_security
-
-
-mysql_database { 'lvm':
-ensure => present,
-charset => 'utf8',
-}
-
-mysql_user { 'indrahpr@indra.com':
-ensure => present,
-max_connections_per_hour => 2,
-}
-
-class {'mymodule::installprog':}
-
+node default {
+hiera_include('myclass')
 
 }
 
