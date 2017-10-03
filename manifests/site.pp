a13a68814f519a877ff5a2b9f0d9a63b7d16ee28
root <root@puppetm.indra.com>
2017-10-03 03:16:06 +0000

    a

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,35 @@
-node default {
-hiera_include('myclass')
+#node default {
+#jhiera_include('myclass')
 
+#}
+
+
+node puppetm {
+    include jenkins
+
+    jenkins::plugin {
+        'ansicolor' :
+            version => '0.3.1';
+    }
+
+    jenkins::job {
+        'build' :
+            config => '<?xml version=\'1.0\' encoding=\'UTF-8\'?>
+<project>
+  <actions/>
+  <description></description>
+  <keepDependencies>false</keepDependencies>
+  <properties></properties>
+  <scm class="hudson.scm.NullSCM"/>
+  <canRoam>true</canRoam>
+  <disabled>false</disabled>
+  <blockBuildWhenDownstreamBuilding>false</blockBuildWhenDownstreamBuilding>
+  <blockBuildWhenUpstreamBuilding>false</blockBuildWhenUpstreamBuilding>
+  <triggers/>
+  <concurrentBuild>false</concurrentBuild>
+  <builders/>
+  <publishers/>
+  <buildWrappers/>
+</project>';
+    }
 }
-
-
